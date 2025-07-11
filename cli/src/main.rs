3ade8be5181e26b759563ade7c7c2ec3da596787
wizardengineer <juliuswoosebert@gmail.com>
2025-07-10 22:28:51 -0400

    more end 2 end testing

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -6,10 +6,16 @@ use bril_passes::{
     compute_liveness, ConstantFoldPass, ConstantPropagationPass, DeadCodeRemovalPass, PassManager,
 };
 
+use riscv_backend::register_alloc;
 use riscv_backend::select_instructions;
+use riscv_backend::Interval;
+use riscv_backend::LinearScan;
+use riscv_backend::MachineFunc;
+use riscv_backend::VReg;
+use std::collections::HashMap;
 
 fn main() -> Result<()> {
-    let json_text = include_str!("../../tests/add.json");
+    let json_text = include_str!("../../tests/factorial.json");
     let bril_prog: Program = serde_json::from_str(&json_text)?;
     let mut ir_mod: IrModule = IrModule::try_from(&bril_prog)?;
     let _ = SSAFormation::try_from(&mut ir_mod)?;
@@ -19,11 +25,22 @@ fn main() -> Result<()> {
     pm.add_pass(DeadCodeRemovalPass {});
     pm.run(&mut ir_mod);
 
-    let (live_out, live_in) = compute_liveness(&ir_mod.functions[0]);
-    println!("{:#?}", live_out);
-    println!("{:#?}", live_in);
+    let mut machine_module = Vec::new();
+    let mut live_intervals = Vec::new();
+    for func in ir_mod.functions.iter() {
+        machine_module.push(select_instructions(func));
+        let (live_out, live_in) = compute_liveness(func);
+        let mut register_alloc = LinearScan::default();
+
+        live_intervals.push(register_alloc.build_intervals(&machine_module.iter().last().unwrap()));
+    }
+
+    //println!("{:#?}", live_out);
+    //println!("{:#?}", live_in);
+    println!("{:#?}", live_intervals);
 
     //println!("{:#?}", ssa);
-    println!("{:#?}", ir_mod);
+    println!("{:#?}\n", ir_mod);
+    println!("{:#?}\n", machine_module);
     Ok(())
 }
